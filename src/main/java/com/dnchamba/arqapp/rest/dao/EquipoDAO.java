42543848d7980c3a62ef5f2fb1fc0866a92412b9
jhuliana <valeriaencalada.11@gmail.com>
2019-02-03 21:55:29 -0500

    Se agrego métodos de archivo EquipoDAO y de EquipoService

--- src/main/java/com/dnchamba/arqapp/rest/dao/EquipoDAO.java
+++ src/main/java/com/dnchamba/arqapp/rest/dao/EquipoDAO.java
@@ -5,10 +5,50 @@
  */
 package com.dnchamba.arqapp.rest.dao;
 
+import com.dnchamba.arqapp.dominio.Conexion;
+import com.dnchamba.arqapp.rest.model.Equipo;
+import java.util.List;
 /**
  *
  * @author Jhuliana
  */
 public class EquipoDAO {
+    private static Conexion usuarios;
+
+    public EquipoDAO() {
+    }
+
+    public EquipoDAO(Conexion usuarios) {
+        this.usuarios = usuarios;
+    }
     
+    public List<Equipo> getDatosEquipo() {
+        return usuarios.getDatosEquipo();
+    }
+
+    public Equipo getEquipo(int id) {
+        return usuarios.getDatoEquipo(id);
+    }
+
+    public Equipo insertarEquipo(Equipo equipo) {
+        usuarios.insertDatosEquipo(equipo);
+        return equipo;
+    }
+
+    public Equipo updateEquipo(Equipo equipo) {
+
+        usuarios.updateDatosEquipo(equipo);
+
+        return equipo;
+    }
+    
+    public boolean deleteEquipo(int id) {
+
+        if (usuarios.delateDatosEquipo(id)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 }
--- src/main/java/com/dnchamba/arqapp/rest/ws/EquipoService.java
+++ src/main/java/com/dnchamba/arqapp/rest/ws/EquipoService.java
@@ -5,10 +5,91 @@
  */
 package com.dnchamba.arqapp.rest.ws;
 
+import com.dnchamba.arqapp.adapter.ConexionMYSQL;
+import com.dnchamba.arqapp.dominio.Conexion;
+import com.dnchamba.arqapp.rest.dao.EquipoDAO;
+import com.dnchamba.arqapp.rest.model.Equipo;
+import java.net.URI;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 /**
  *
  * @author Jhuliana
  */
+@Path("equipo")
 public class EquipoService {
+
+    private static Conexion usuario = new ConexionMYSQL();
+    private static EquipoDAO equipoDAO = new EquipoDAO(usuario);
+
+
     
+
+    public EquipoService() {
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Equipo> getAllEquipos(){
+        return equipoDAO.getDatosEquipo();
+    }     
+    
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getEquipo(@PathParam("id") int id){
+        Equipo output = equipoDAO.getEquipo(id);
+        
+        if (output != null) {
+            return Response.ok(output).build();
+            
+        }else{
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addEquipo(Equipo equipo, @Context UriInfo uriInfo){
+        Equipo newEquipo = equipoDAO.insertarEquipo(equipo);
+        
+        UriBuilder uriBuilder = uriInfo.getAbsolutePathBuilder();
+        
+        URI newUri = uriBuilder.path(String.valueOf(newEquipo.getId_equipo())).build();
+           
+        return Response.created(newUri).entity(newEquipo).build();
+    }
+    
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Equipo updateEquipo(@PathParam("id")int id, Equipo equipo){
+        equipo.setId_equipo(id);
+        return equipoDAO.updateEquipo(equipo);
+    }
+    
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response remove(@PathParam("id") int id){
+        if (equipoDAO.deleteEquipo(id)){
+            
+            return Response.noContent().build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }    
 }
