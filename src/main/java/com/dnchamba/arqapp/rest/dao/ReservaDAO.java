02fe5140a26e3ae2992ff02492654c7f983ec962
jhuliana <valeriaencalada.11@gmail.com>
2019-02-03 22:00:35 -0500

    Se agrego métodos de archivo ReservaDAO

--- src/main/java/com/dnchamba/arqapp/rest/dao/ReservaDAO.java
+++ src/main/java/com/dnchamba/arqapp/rest/dao/ReservaDAO.java
@@ -5,10 +5,50 @@
  */
 package com.dnchamba.arqapp.rest.dao;
 
+import com.dnchamba.arqapp.dominio.Conexion;
+import com.dnchamba.arqapp.rest.model.Ensayo;
+import com.dnchamba.arqapp.rest.model.Reserva;
+import java.util.List;
+
 /**
  *
  * @author Jhuliana
  */
 public class ReservaDAO {
+        private static Conexion usuarios;
+
+    public ReservaDAO(Conexion usuarios) {
+        this.usuarios = usuarios;
+    }
     
+    public List<Reserva> getDatosReserva() {
+        return usuarios.getDatosReserva();
+    }
+
+    public Reserva getReserva(int id) {
+        return usuarios.getDatoReserva(id);
+    }
+
+    public Reserva insertarReserva(Reserva reserva) {
+        usuarios.insertDatosReserva(reserva);
+        return reserva;
+    }
+
+    public Reserva updateReserva(Reserva reserva) {
+
+        usuarios.updateDatosReserva(reserva);
+
+        return reserva;
+    }
+    
+    public boolean deleteReserva(int id) {
+
+        if (usuarios.delateDatosReserva(id)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
 }
